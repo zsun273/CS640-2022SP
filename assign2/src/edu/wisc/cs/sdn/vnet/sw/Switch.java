e4f8cff347bf4cda345992f21e4317f442db6fdc
Leo Sun <zsun273@wisc.edu>
2022-02-26 22:41:48 -0600

    Switch.java implementation for test

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,15 +1,45 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.MACAddress;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.System;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
-{	
+{
+	Map<MACAddress, SwitchEntry> SwitchMap = new HashMap<MACAddress, SwitchEntry>();
+
+	/**
+	 * @author Zhuocheng Sun
+	 */
+	static class SwitchEntry {
+		private MACAddress destination;
+		private Iface inIface;			// the interface on which the packet was received
+		long TTL;						// time to live
+
+		public SwitchEntry(MACAddress destination, Iface inIface) {
+			this.destination = destination;
+			this.inIface = inIface;
+			this.TTL = System.currentTimeMillis();
+		}
+
+		public Iface getInIface() {
+			return inIface;
+		}
+
+		public long getTTL() {
+			return TTL;
+		}
+	}
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -28,10 +58,44 @@ public class Switch extends Device
 	{
 		System.out.println("*** -> Received packet: " +
 				etherPacket.toString().replace("\n", "\n\t"));
-		
+
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
-		
+		MACAddress sourceMAC;
+		MACAddress destinationMAC;
+		sourceMAC = etherPacket.getSourceMAC();
+		destinationMAC = etherPacket.getDestinationMAC();
+
+		SwitchMap.put(sourceMAC, new SwitchEntry(sourceMAC, inIface));
+		// find timed out entries and delete them from the switch map
+		for (Map.Entry<MACAddress, SwitchEntry> entry: SwitchMap.entrySet()){
+			if (System.currentTimeMillis() - entry.getValue().getTTL() >= 15000) {
+				SwitchMap.remove(entry.getKey());
+			}
+		}
+
+		System.out.println("\n Current entries in switch map\n");
+		for (Map.Entry<MACAddress, SwitchEntry> entry: SwitchMap.entrySet()){
+			System.out.println(entry.getKey() + ": " + entry.getValue().getInIface() + "," + entry.getValue().getTTL());
+		}
+
+		if (SwitchMap.containsKey(destinationMAC)) {
+			System.out.println("Destination in the Map\n");
+			if (this.sendPacket(etherPacket, SwitchMap.get(destinationMAC).getInIface()) == false){
+				System.out.println("Send failed");
+			}
+			return;
+		}
+		else{
+			Map<String,Iface> interfaces = this.interfaces;
+			for (Map.Entry<String, Iface> entry : interfaces.entrySet()){
+				System.out.println("Sending: " + destinationMAC + ": " + entry.getValue());
+				if(this.sendPacket(etherPacket, entry.getValue())){
+					System.out.println("Send successful");
+				}
+			}
+			return;
+		}
 		/********************************************************************/
 	}
 }
