10b6be1ab530e8efba5ebe07ff93cae6b46c4fb5
Leo Sun <zsun273@wisc.edu>
2022-04-25 14:31:33 -0500

    add synchronization for two threads in sender

--- assign4/src/Sender.java
+++ assign4/src/Sender.java
@@ -447,10 +447,9 @@ public class Sender {
                 byte[] incomingData = new byte[24]; // receiver will not send data back
                 DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
 
-                synchronized (this){
-
-
                 try {
+                    synchronized (this) {
+
                     while (stopSend == false) {
                         System.out.println("Sender: Receiving thread receiving......");
                         senderSocket.receive(incomingPacket);
@@ -529,81 +528,45 @@ public class Sender {
                 try {
                     synchronized (this) {
 
-                    while (stopSend == false) {
+                        while (stopSend == false) {
 
-                        // wait for connection established
-                        // System.out.println("open: " + open + " finalPacket: " + finalPacket);
-                        if (open == true && finalPacket == false) { // send data
-                            System.out.println("Sender: Sending thread sending......");
-                            // determine how many bytes to send OR wait
-                            long remainingBytes = file.length() - 1 - lastSent;
-                            int swCapacity = sws - (lastSent - lastAcked);
+                            // wait for connection established
+                            // System.out.println("open: " + open + " finalPacket: " + finalPacket);
+                            if (open == true && finalPacket == false) { // send data
+                                System.out.println("Sender: Sending thread sending......");
+                                // determine how many bytes to send OR wait
+                                long remainingBytes = file.length() - 1 - lastSent;
+                                int swCapacity = sws - (lastSent - lastAcked);
 
-                            // data can be sent would be the min(remaining, swCapacity, mtu)
-                            int sentSize = Math.min((int) remainingBytes, swCapacity);
-                            sentSize = Math.min(sentSize, mtu);
-                            if (sentSize > 0) {
-                                if (sentSize == remainingBytes)
-                                    finalPacket = true;
+                                // data can be sent would be the min(remaining, swCapacity, mtu)
+                                int sentSize = Math.min((int) remainingBytes, swCapacity);
+                                sentSize = Math.min(sentSize, mtu);
+                                if (sentSize > 0) {
+                                    if (sentSize == remainingBytes)
+                                        finalPacket = true;
 
-                                byte[] data = new byte[sentSize];
-                                if (fileReader.read(data) != -1) {
-                                    ArrayList<Integer> flagBits = new ArrayList<>();
-                                    flagBits.add(ACK);
-                                    byte[] packet = createPacket(lastSent + 1, data, flagBits);
+                                    byte[] data = new byte[sentSize];
+                                    if (fileReader.read(data) != -1) {
+                                        ArrayList<Integer> flagBits = new ArrayList<>();
+                                        flagBits.add(ACK);
+                                        byte[] packet = createPacket(lastSent + 1, data, flagBits);
 
-                                    DatagramPacket udpPacket = new DatagramPacket(packet, packet.length, InetAddress.getByName(remoteIP), receiverPort);
-                                    senderSocket.send(udpPacket);
-                                    setTimeOut(lastSent + 1, packet);
-                                    output(packet, true);
-                                    updateAfterSend(data);
+                                        DatagramPacket udpPacket = new DatagramPacket(packet, packet.length, InetAddress.getByName(remoteIP), receiverPort);
+                                        senderSocket.send(udpPacket);
+                                        setTimeOut(lastSent + 1, packet);
+                                        output(packet, true);
+                                        updateAfterSend(data);
+                                    }
                                 }
+
+
+                                // print stats for debugging
+                                System.out.println("remain: " + remainingBytes + ", swCapacity: " + swCapacity);
+
+
                             }
-
-
-                            // print stats for debugging
-                            System.out.println("remain: " + remainingBytes + ", swCapacity: " + swCapacity);
-
-//                            if (remainingBytes <= mtu && remainingBytes <= swCapacity) { // send in one transmission
-//                                finalPacket = true;  // can we set finalPacket to true at this point?
-//                                byte[] data = new byte[(int) remainingBytes];
-//                                if (fileReader.read(data) != -1) {
-//                                    ArrayList<Integer> flagBits = new ArrayList<>();
-//                                    flagBits.add(ACK);
-//                                    byte[] packet = createPacket(lastSent + 1, data, flagBits);
-//
-//                                    DatagramPacket udpPacket = new DatagramPacket(packet, packet.length, InetAddress.getByName(remoteIP), receiverPort);
-//                                    senderSocket.send(udpPacket);
-//                                    setTimeOut(lastSent + 1, packet);
-//                                    output(packet, true);
-//                                    updateAfterSend(data);
-//                                }
-//                            }
-//
-//                            else if (remainingBytes > mtu) {  // send in multiple runs
-//                                if (mtu > swCapacity)         // cannot fit in buffer, wait
-//                                    continue;
-//                                else {                        // can send a packet of size mtu
-//                                    byte[] data = new byte[mtu];
-//                                    if (fileReader.read(data) != -1) { // read successfully
-//                                        ArrayList<Integer> flagBits = new ArrayList<>();
-//                                        flagBits.add(ACK);
-//                                        byte[] packet = createPacket(lastSent + 1, data, flagBits);
-//
-//                                        DatagramPacket udpPacket = new DatagramPacket(packet, packet.length, InetAddress.getByName(remoteIP), receiverPort);
-//                                        senderSocket.send(udpPacket);
-//                                        setTimeOut(lastSent + 1, packet);
-//                                        output(packet, true);
-//                                        updateAfterSend(packet);
-//                                    }
-//                                }
-//                            }
                         }
-//			else{
-//				continue;
-//			}
                     }
-                }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
