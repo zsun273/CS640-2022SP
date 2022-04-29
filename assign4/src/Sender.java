82f1c2c138307d376128fdbc9590da3759a6ae03
xiong326 <xiong73@wisc.edu>
2022-04-29 17:23:03 -0500

    revert

--- assign4/src/Sender.java
+++ assign4/src/Sender.java
@@ -284,8 +284,6 @@ public class Sender {
         //Timer timer = new Timer();
 
         // cancel previous timer on the same packet if any
-        if (seqNum < lastAcked)
-            return;
         TimerTask preTimer = timerMap.get(seqNum);
         if (preTimer != null) {
             preTimer.cancel();
